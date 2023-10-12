ca47478baf6b5d18a6fd098967292586d8616ab4
ArtemV <artemvorohobko@gmail.com>
2023-10-13 02:05:32 +0300

    I have finished sub-task 2 of Task2

--- src/homework/day4/playground/essence/craft/air/Aircraft.java
+++ src/homework/day4/playground/essence/craft/air/Aircraft.java
@@ -3,4 +3,17 @@ package homework.day4.playground.essence.craft.air;
 import homework.day4.playground.essence.Matter;
 
 public abstract class Aircraft extends Matter {
+    protected String name;
+
+    public Aircraft (String name, int mass) {
+        super (mass);
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
 }
--- src/homework/day4/playground/essence/craft/hand/Container.java
+++ src/homework/day4/playground/essence/craft/hand/Container.java
@@ -3,4 +3,16 @@ package homework.day4.playground.essence.craft.hand;
 import homework.day4.playground.essence.Matter;
 
 public abstract class Container extends Matter {
+    protected String name;
+
+    public Container (String name, int mass) {
+        super (mass);
+        this.name = name;
+    }
+    public String getName () {
+        return name;
+    }
+     public void setName (String name){
+        this.name = name;
+    }
 }
