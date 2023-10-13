db834973dee7c3790f594e76a934c3e7d199ad23
ArtemV <artemvorohobko@gmail.com>
2023-10-13 18:09:47 +0300

    I have finished sub-task4 of Task2

--- src/homework/day4/playground/essence/creatures/Vertebrata.java
+++ src/homework/day4/playground/essence/creatures/Vertebrata.java
@@ -4,4 +4,9 @@ public abstract class Vertebrata extends Animal {
     public Vertebrata(String name, int mass) {
         super(name, mass);
     }
+
+    public void eat(Insect food) {
+        String str = String.format("I am %s and I am eating %s", this.name, food.getName());
+        System.out.println(str);
+    }
 }
