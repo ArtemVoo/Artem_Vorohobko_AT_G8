d966185806172add43030fc7b94aec130fce21f7
ArtemV <artemvorohobko@gmail.com>
2023-10-21 02:46:47 +0300

    I finished sub-task 3 of String task

---
+++ src/homework/day5/String_task/FindDigitsInText.java
@@ -0,0 +1,11 @@
+package homework.day5.String_task;
+
+public class FindDigitsInText {
+    public void findDigits(String text){
+        for(char x : text.toCharArray()){
+            if(Character.isDigit(x)){
+                System.out.println(x);
+            }
+        }
+    }
+}
