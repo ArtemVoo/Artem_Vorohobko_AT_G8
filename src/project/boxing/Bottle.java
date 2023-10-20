8a9fafcd17f32f0ee9bea75500fbb5128babe856
ArtemV <artemvorohobko@gmail.com>
2023-10-21 02:43:40 +0300

    Project "Bubble" - not finished

--- src/project/boxing/Bottle.java
+++ src/project/boxing/Bottle.java
@@ -1,5 +1,44 @@
 package project.boxing;
 
-public class Bottle {
+import project.water.Bubble;
+import project.water.Water;
 
+public class Bottle {
+    private double volume;
+    private Water water;
+
+    public Bottle(double volume) {
+        this.volume = volume;
+    }
+
+    public void open() throws InterruptedException {
+        if (water != null) {
+            water.setOpened();
+        }
+    }
+
+    public void warm(int temperature) {
+        if (water != null) {
+            water.setTemperature(temperature);
+        }
+    }
+    public Water getWater() {
+        return water;
+    }
+
+    public Water setWater(Water water) {
+        this.water = water;
+        return  water;
+    }
+
+    public void setBubbles() {
+        if(water != null){
+            int numOfBubbles = (int) (volume * 1000); //??
+            Bubble[] bubbles = new Bubble[numOfBubbles];
+            for(int i = 0; i < numOfBubbles; i++){
+                bubbles[i] = new Bubble("Gas");
+            }
+            water.pump();
+        }
+    }
 }
--- src/project/water/Bubble.java
+++ src/project/water/Bubble.java
@@ -1,4 +1,18 @@
 package project.water;
 
 public class Bubble {
+    private double volume = 0.3;
+    private String gas;
+
+    public Bubble(String gas) {
+        this.gas = gas;
+    }
+
+    public void cramp() {
+        System.out.println("Cramp!");
+    }
+
+    public double getVolume() {
+        return volume;
+    }
 }
