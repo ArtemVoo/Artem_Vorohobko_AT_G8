c0251f58c66e5eb0b064b526e037d4fb5da7616a
ArtemV <artemvorohobko@gmail.com>
2023-10-21 02:46:11 +0300

    I finished sub-task 2 of String task

---
+++ src/homework/day5/String_task/AllDuplicates.java
@@ -0,0 +1,37 @@
+package homework.day5.String_task;
+
+public class AllDuplicates {
+    public void findDuplicateWords(String text) {
+        String[] words = text.split(" ");
+        String[] duplicates = new String[words.length];
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            boolean isDuplicate = false;
+
+            for (int x = i + 1; x < words.length; x++) {
+                if (word.equals(words[x])) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (isDuplicate) {
+                boolean added = false;
+                for (int y = 0; y < count; y++) {
+                    if (word.equals(duplicates[y])) {
+                        added = true;
+                        break;
+                    }
+                }
+                if (!added) {
+                    duplicates[count] = word;
+                    count++;
+                }
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(duplicates[i]);
+        }
+    }
+}
