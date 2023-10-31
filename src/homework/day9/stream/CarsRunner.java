a4157f95c2dcb770038a208cebc6e29be4573d94
ArtemV <artemvorohobko@gmail.com>
2023-10-31 23:42:42 +0300

    Minor fix in the task "4"

--- src/homework/day9/stream/CarsRunner.java
+++ src/homework/day9/stream/CarsRunner.java
@@ -5,9 +5,8 @@ import java.util.stream.*;
 public class CarsRunner {
 
     public static void main(String[] args) {
-        Stream<String> cars
-                = Stream.of("Мерс", "Ауди", "Жигуль", "Рено", "Жигуль", "Жигуль", "Ауди").distinct()
-                .filter(x -> x.contains("и")).skip(1).map(String::toUpperCase);
-        cars.forEach(x -> System.out.println(x + " "));
+        Stream.of("Мерс", "Ауди", "Жигуль", "Рено", "Жигуль", "Жигуль", "Ауди").distinct()
+                .filter(x -> x.contains("и")).skip(1).map(String::toUpperCase)
+                .forEach(x -> System.out.println(x + " "));
     }
 }
