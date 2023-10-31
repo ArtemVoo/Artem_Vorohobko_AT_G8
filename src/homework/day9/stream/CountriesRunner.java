389ba971e24cf71a4f7e0525d7c1c9c468324d6e
ArtemV <artemvorohobko@gmail.com>
2023-10-31 23:43:09 +0300

    Minor fix in the task "5"

--- src/homework/day9/stream/CountriesRunner.java
+++ src/homework/day9/stream/CountriesRunner.java
@@ -5,10 +5,8 @@ import java.util.stream.Stream;
 public class CountriesRunner {
 
     public static void main(String[] args) {
-        Stream<String> countries
-                = Stream.of("Андора", "Португалия", "Англия", "Замбия")
+        Stream.of("Андора", "Португалия", "Англия", "Замбия")
                 .filter(x -> x.matches(".*[аеёиоуыэюя].*")).filter(x -> x.length() < 7)
-                .map(String::toUpperCase).map(x -> ("\"" + x + "\""));
-        countries.forEach(x -> System.out.println(x + " "));
+                .map(String::toUpperCase).map(x -> ("\"" + x + "\"")).forEach(x -> System.out.println(x + " "));
     }
 }
